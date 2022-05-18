dcec5eba339867a3c4e563d89be132564360b39f
Meir Ben-Shimon <Meir.b@qualisystems.com>
2022-05-18 17:54:49 +0300

    move init code to a dedicated file

--- PuzoLabs.Hamikdash.Reservations.Tests/DatabaseTests.cs
+++ PuzoLabs.Hamikdash.Reservations.Tests/DatabaseTests.cs
@@ -1,15 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PuzoLabs.Hamikdash.Reservations.Db;
 using PuzoLabs.Hamikdash.Reservations.Db.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace PuzoLabs.Hamikdash.Reservations.Tests
@@ -17,35 +10,14 @@ namespace PuzoLabs.Hamikdash.Reservations.Tests
     [TestClass]
     public class DatabaseTests
     {
-        private IConfigurationRoot _config;
         IOptions<DbSettings> _options;
 
         [TestInitialize]
         public async Task InitConfiguration()
         {
-            var services = new ServiceCollection();
-
-            // IOption configuration injection
-            services.AddOptions();
-
-            services.AddLogging(logging => logging.AddConsole());
-
-            _config = new ConfigurationBuilder()
-                .AddJsonFile("appSettings.test.json")
-                //.AddEnvironmentVariables()
-                .Build();
-
-            services.Configure<DbSettings>(_config.GetSection($"{DbSettings.SettingsKeyName}"));
-            services.AddSingleton<IHostedService, DatabaseMigrator>();
-
-            ServiceProvider serviceProvider = services.BuildServiceProvider();
-
-            _options = serviceProvider.GetRequiredService<IOptions<DbSettings>>();
-
-            //migrate db
-            var migrator = serviceProvider.GetRequiredService<IHostedService>();
-            //IHostedService migrator = new DatabaseMigrator(_options, null);
-            await migrator.StartAsync(new CancellationToken());
+            InitManager initManager = new InitManager();
+            await initManager.Init();
+            _options = initManager.DbOptions;
         }
 
         [TestMethod]
---
+++ PuzoLabs.Hamikdash.Reservations.Tests/InitManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Threading;
+using System.Threading.Tasks;
+using PuzoLabs.Hamikdash.Reservations.Db;
+
+namespace PuzoLabs.Hamikdash.Reservations.Tests
+{
+    public class InitManager
+    {
+        public IOptions<DbSettings> DbOptions { get; private set; }
+
+        public async Task Init()
+        {
+            var services = new ServiceCollection();
+
+            // IOption configuration injection
+            services.AddOptions();
+
+            services.AddLogging(logging => logging.AddConsole());
+
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.test.json")
+                //.AddEnvironmentVariables()
+                .Build();
+
+            services.Configure<DbSettings>(config.GetSection($"{DbSettings.SettingsKeyName}"));
+            services.AddSingleton<IHostedService, DatabaseMigrator>();
+
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            DbOptions = serviceProvider.GetRequiredService<IOptions<DbSettings>>();
+
+            //migrate db
+            var migrator = serviceProvider.GetRequiredService<IHostedService>();
+            await migrator.StartAsync(new CancellationToken());
+        }
+    }
+}
